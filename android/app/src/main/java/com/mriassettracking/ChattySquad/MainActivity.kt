353c4f2e5573f31866e1d6745f619bb918ded785
Kaushal Vasava <kaushal.vasava@mrisoftware.com>
2025-07-23 11:59:33 +0530

    Update package name (#4)

--- android/app/src/main/java/com/mriassettracking/ChattySquad/MainActivity.kt
+++ android/app/src/main/java/com/mriassettracking/ChattySquad/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.ChattySquad
+package com.mriassettracking.ChattySquad
 
 import android.os.Build
 import android.os.Bundle
@@ -7,6 +7,8 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.mriassettracking.BuildConfig
+import com.mriassettracking.R
 
 import expo.modules.ReactActivityDelegateWrapper
 
@@ -33,7 +35,7 @@ class MainActivity : ReactActivity() {
   override fun createReactActivityDelegate(): ReactActivityDelegate {
     return ReactActivityDelegateWrapper(
           this,
-          BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+        BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
           object : DefaultReactActivityDelegate(
               this,
               mainComponentName,
--- android/app/src/main/java/com/mriassettracking/ChattySquad/MainApplication.kt
+++ android/app/src/main/java/com/mriassettracking/ChattySquad/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.mriassettracking
+package com.mriassettracking.ChattySquad
 
 import android.app.Application
 import android.content.res.Configuration
@@ -12,6 +12,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+import com.mriassettracking.BuildConfig
 
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
